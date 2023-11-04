c27e8188deefe777f6bef3d46aef72d36006e230
Mccullahz <zylardmccullah@gmail.com>
2023-11-04 16:47:28 -0400

    Added Chrome and neovim installs

--- firstinstalls.sh
+++ firstinstalls.sh
@@ -2,7 +2,13 @@
 
 sudo apt update
 sudo apt upgrade
-sudo apt install vim
+sudo apt install neovim
+
+
+# Install Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
 
 # Python & pip
 sudo apt install python3-pip
